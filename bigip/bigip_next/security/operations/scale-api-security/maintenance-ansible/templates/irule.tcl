4043f538587c1a990ce5cd1e579a60f039ba7339
Serge Levin <se.levin@f5.com>
2025-01-15 19:35:19 +0000

    update rule to be Avoid Traversal Attack rule

--- bigip/bigip_next/security/operations/scale-api-security/maintenance-ansible/templates/irule.tcl
+++ bigip/bigip_next/security/operations/scale-api-security/maintenance-ansible/templates/irule.tcl
@@ -1,5 +1,32 @@
-when HTTP_REQUEST {
-  if { [HTTP::host] eq "app.domain.local" } {
-    pool app-maintenance-pool
+when HTTP_REQUEST { 
+  set file ""
+  if {[regexp {filename=([^&]+)} [HTTP::uri] match value]} {
+      set file $value
+  }
+  
+  if { ($file starts_with "/") or ($file starts_with "../") } { 
+    log local0. "[IP::client_addr] requested $file" 
+    HTTP::respond 403 content "I'm sorry, but your request for $file is contains invalid characters. Please try your request again.\n" 
+    HTTP::close
+    return
   } 
+
+  if { [HTTP::method] equals "POST" } {
+    HTTP::collect 256
+  } 
+} 
+
+when HTTP_REQUEST_DATA { 
+  set file ""
+  if {[regexp {filename=([^&]+)} [HTTP::payload] match value]} {
+      set file $value
+  }
+  
+  if { ($file starts_with "/") or ($file starts_with "../") } { 
+    log local0. "[IP::client_addr] requested $file" 
+    HTTP::respond 403 content "I'm sorry, but your request for $file is contains invalid characters. Please try your request again.\n" 
+    HTTP::close 
+  } 
+
+  HTTP::release 
 }
